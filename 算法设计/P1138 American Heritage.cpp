cf1c4bdc8923d41b3c21b3cda5f2eae81f0160ef
cryingingingstar <15028911962@163.com>
2025-03-12 12:31:11 +0800

    code

--- 算法设计/P1138 American Heritage.cpp
+++ 算法设计/P1138 American Heritage.cpp
@@ -1,32 +1,47 @@
-
-//给出前序和中序遍历，求后序遍历
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-typedef struct node{
-    char c;//保存奶牛编号
-    struct node* left;
-    struct node* right;
-}node,*Bitree;
 
-//递归函数
-void Post_list(vector<int> root,string mid_list,Bitree t,int r,int start,int end){
-    t->c=mid_list[root[r]];
-    Post_list(root,mid_list,t->left,++r,start,root[r]-1);
-    Post_list(root,mid_list,t->right,)
-
+// 后序遍历的递归函数
+string postOrder(const string& inorder, const string& preorder) {
+    // 递归的终止条件：如果没有节点，返回空字符串
+    if (inorder.empty()) {
+        return "";
+    }
+    
+    // 前序遍历的第一个字符是根节点
+    char root = preorder[0];
+    
+    // 在中序遍历中找到根节点的位置
+    int rootPos = inorder.find(root);
+    
+    // 左子树的中序遍历是根节点左边的部分
+    string leftInorder = inorder.substr(0, rootPos);
+    // 右子树的中序遍历是根节点右边的部分
+    string rightInorder = inorder.substr(rootPos + 1);
+    
+    // 左子树的前序遍历是根节点后的部分（与左子树的中序部分大小相等）
+    string leftPreorder = preorder.substr(1, leftInorder.size());
+    // 右子树的前序遍历是剩下的部分
+    string rightPreorder = preorder.substr(1 + leftInorder.size());
+    
+    // 递归求得左子树和右子树的后序遍历
+    string leftPostorder = postOrder(leftInorder, leftPreorder);
+    string rightPostorder = postOrder(rightInorder, rightPreorder);
+    
+    // 后序遍历的顺序是：左子树 + 右子树 + 根节点
+    return leftPostorder + rightPostorder + root;
 }
 
-int main(){
-    Bitree t;
-    vector<int> root(10000);
-    string pre_list,mid_list;
-    cin>>pre_list>>mid_list;
-    t=new node;
+int main() {
+    string inorder, preorder;
+    cin >> inorder;  // 中序遍历
+    cin >> preorder; // 前序遍历
     
-    for(int i=0;i<pre_list.length();i++){
-        int r=mid_list.find(pre_list[i]);
-        root[i]=r;
-    }
+    // 计算并输出后序遍历
+    string postorder = postOrder(inorder, preorder);
+    cout << postorder << endl;
+    
+    return 0;
 }
--- 算法设计/test.cpp
+++ 算法设计/test.cpp
@@ -1,47 +1,68 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-typedef long long ll;
+// 辅助函数：根据前序和中序遍历构造后序遍历
+vector<char> buildPostorder(const vector<char>& preorder, const vector<char>& inorder, int preStart, int preEnd, int inStart, int inEnd, unordered_map<char, int>& inMap) {
+    // 如果没有元素，返回空
+    if (preStart > preEnd || inStart > inEnd) {
+        return {};
+    }
+    
+    // 根节点是前序遍历的第一个元素
+    char root = preorder[preStart];
+    
+    // 在中序遍历中找到根节点的索引
+    int rootIndex = inMap[root];
+    
+    // 左子树的大小
+    int leftSize = rootIndex - inStart;
+    
+    // 递归处理左子树和右子树的后序遍历
+    vector<char> leftPostorder = buildPostorder(preorder, inorder, preStart + 1, preStart + leftSize, inStart, rootIndex - 1, inMap);
+    vector<char> rightPostorder = buildPostorder(preorder, inorder, preStart + leftSize + 1, preEnd, rootIndex + 1, inEnd, inMap);
+    
+    // 后序遍历：左子树的后序 + 右子树的后序 + 根节点
+    leftPostorder.insert(leftPostorder.end(), rightPostorder.begin(), rightPostorder.end());
+    leftPostorder.push_back(root);
+    
+    return leftPostorder;
+}
 
-// 计算至少需要多少次 buff 才能超过怪物的血量位数
-int f(ll x, int life) {
-    int count = 1;
-    // 计算初始伤害的位数
-    double w = log10(x);
-    
-    // 如果初始伤害的位数已经超过怪物血量的位数，返回 0
-    if (w > life) {
-        return 0;
+// 主函数：调用递归函数来构造后序遍历
+vector<char> postorderTraversal(const vector<char>& preorder, const vector<char>& inorder) {
+    unordered_map<char, int> inMap;  // 用于存储中序遍历元素的位置
+    for (int i = 0; i < inorder.size(); ++i) {
+        inMap[inorder[i]] = i;
     }
     
-    for (;;) {
-        if (count % 2 == 1) {  // 如果是奇数次 buff
-            w += log10(x);
-        } else {  // 如果是偶数次 buff
-            w += log10(x) + log10(count);
-        }
-        
-        // 如果伤害的位数超过怪物血量的位数，返回施放的次数
-        if (w > life) {
-            return count;
-        }
-        
-        // 增加 buff 次数
-        count++;
-    }
+    // 调用递归函数
+    return buildPostorder(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1, inMap);
 }
 
 int main() {
-    ll x;
-    int n;
-    cin >> x >> n;
+    // 输入前序遍历
+    cout << "请输入前序遍历（字符组成）: ";
+    string preorder_str;
+    cin >> preorder_str;
+    vector<char> preorder(preorder_str.begin(), preorder_str.end());  // 将输入的字符串转换为字符数组
     
-    for (int i = 0; i < n; i++) {
-        int life;
-        cin >> life;
-        cout << f(x, life) << endl;
+    // 输入中序遍历
+    cout << "请输入中序遍历（字符组成）: ";
+    string inorder_str;
+    cin >> inorder_str;
+    vector<char> inorder(inorder_str.begin(), inorder_str.end());  // 将输入的字符串转换为字符数组
+    
+    // 获取后序遍历
+    vector<char> postorder = postorderTraversal(preorder, inorder);
+    
+    // 输出后序遍历
+    cout << "后序遍历为: ";
+    for (char val : postorder) {
+        cout << val << " ";
     }
+    cout << endl;
     
     return 0;
 }
